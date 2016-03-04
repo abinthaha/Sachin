626b44050a7015821916e22825eef42d75d4b072
Abin Thaha Azeez <abin@qburst.com>
2016-03-04 18:42:57 +0530

    Added records

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,61 @@ angular.module('sachin', [])
 
 		$scope.showLoader = true;
 		$http.get('assets/json/sachin.json').success(function(data){
-			_.each(data, function(a){
-				if (a.runs_conceded != '-' && a.runs_conceded > 0) {
-					$scope.tableData.push(a);
-				}
-			})
+			$scope.tableData = data;
 			$scope.showLoader = false;
 		});
+		$http.get('assets/json/records.json').success(function(data){
+			$scope.recordData = data;
+			$scope.showLoader = false;
+		});
+		$scope.resultButtons = [
+			'Won Matches', 'Total Runs', 'Runs Conceded', 'Total Wickets', 'Out in 0'
+		]
+		$scope.populateResults = function(index){
+			var count = 0;
+			switch (index) {
+				case 0:
+					_.each($scope.tableData, function (row) {
+						if(row.match_result == "won")
+							count = count+1;
+						$scope.result_input = count;
+					})
+					break;
+				case 1:
+					_.each($scope.tableData, function (row) {
+						var val = _.isFinite(row.batting_score) ? JSON.parse(row.batting_score) : 0;
+						count = count+val;
+						$scope.result_input = count;
+					})
+					break;
+				case 2:
+					_.each($scope.tableData, function (row) {
+						var val = _.isFinite(row.runs_conceded) ? JSON.parse(row.runs_conceded) : 0;
+						count = count+val;
+						$scope.result_input = count;
+					})
+					break;
+				case 3:
+					_.each($scope.tableData, function (row) {
+						var val = _.isFinite(row.wickets) ? JSON.parse(row.wickets) : 0;
+						count = count+val;
+						$scope.result_input = count;
+					})
+					break;
+				case 4:
+					_.each($scope.tableData, function (row) {
+						if (_.isFinite(row.batting_score)) {
+							if (JSON.parse(row.batting_score) == 0) {
+								count += 1;
+							}
+						}
+						$scope.result_input = count;
+					})
+					break;
+				default:
 
+			}
+		}
 		$scope.$watch('tableData', function(newValue, oldValue){
 			$scope.showLoader = false;
 		})
